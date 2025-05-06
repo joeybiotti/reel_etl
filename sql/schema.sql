c8f436711c69b5ac7002c1c339a42673a19fae93
Joey Biotti <joeybiotti@Joeys-MacBook-Pro.local>
2025-05-05 22:35:56 -0400

    Add SQL queries and schema for Movies dataset in REEL ETL project

---
+++ sql/schema.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE movies (
+    color TEXT,
+    director_name TEXT,
+    num_critic_for_reviews INTEGER,
+    duration REAL,
+    director_facebook_likes INTEGER,
+    actor_3_facebook_likes INTEGER,
+    actor_2_name TEXT,
+    actor_1_facebook_likes INTEGER,
+    gross REAL,
+    genres TEXT,
+    actor_1_name TEXT,
+    movie_title TEXT,
+    num_voted_users INTEGER,
+    cast_total_facebook_likes INTEGER,
+    actor_3_name TEXT,
+    facenumber_in_poster INTEGER,
+    plot_keywords TEXT,
+    movie_imdb_link TEXT,
+    num_user_for_reviews INTEGER,
+    language TEXT,
+    country TEXT,
+    content_rating TEXT,
+    budget REAL,
+    title_year INTEGER,
+    actor_2_facebook_likes INTEGER,
+    imdb_score REAL,
+    aspect_ratio REAL,
+    movie_facebook_likes INTEGER
+);
