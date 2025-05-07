f74290264b5b8cd5e2bdcd094ecb3d5cf3edc83c
Joey Biotti <joeybiotti@Joeys-MacBook-Pro.local>
2025-05-07 11:02:37 -0400

    Add tests for save_processed and load functions in ETL pipeline

--- tests/test_etl_pipeline.py
+++ tests/test_etl_pipeline.py
@@ -1,6 +1,7 @@
 import pytest
 import pandas as pd
-from scripts.etl_pipeline import extract, transform, normalize_text_fields
+import sqlite3
+from scripts.etl_pipeline import extract, transform, normalize_text_fields,save_processed,load
 
 @pytest.fixture
 def sample_df():
@@ -45,3 +46,34 @@ def test_normalize_text_fields(sample_df):
     assert all(normalized_df['genres'].str.islower())
     assert all(normalized_df['language'].str.islower())
     assert all(normalized_df['country'].str.islower())
+    
+def test_save_processed_creates_files(sample_df, tmp_path):
+    """Test that save_process correctly saves a CSV file"""
+    output_file = tmp_path/"test_output.csv"
+    save_processed(sample_df, output_file)
+    
+    # Check file was created
+    assert output_file.exists()
+    
+    # Check file is not empty
+    saved_df = pd.read_csv(output_file)
+    assert not saved_df.empty
+    assert list(saved_df.columns) == list(sample_df.columns)
+    
+def test_load_inserts_data_into_database(sample_df, tmp_path):
+    """Test correctly inserts data into SQLite db"""
+    db_file = tmp_path/"test_movies.db"
+    table_name = "test_movies"
+    
+    # Load sample data into temp db
+    load(sample_df, db_file, table_name)
+    
+    # Query db to confirm insert
+    conn = sqlite3.connect(db_file)
+    result_df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
+    conn.close()
+    
+    # Verify data is loaded
+    assert not result_df.empty
+    assert set(result_df.columns) == set(sample_df.columns)
+    assert len(result_df) == len(sample_df)
